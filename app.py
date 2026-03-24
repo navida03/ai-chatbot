b670bae9230839a70bd1d345eb674f519c3b15a7
Navida Rajbhoj <navidarajbhoj03@gmail.com>
2026-03-24 14:30:47 +0530

    fix requirements for render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import json
 import random
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def chat():
     return jsonify({"response": "Sorry, I didn’t understand."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
